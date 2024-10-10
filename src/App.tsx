1ef76c5589cecc5f3baf9a347dc0551f5cb124b6
kamelmaher <kamelmahersh@gmail.com>
2024-10-10 11:31:44 +0300

    Admin User

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from './Store/Store'
 
 function App() {
   // Comment after Commit 
+  // Use email: admin123@gmail.com  password: pass123 to login as Admin
   const books = useAppSelector(state => state.Book.books)
   const [filteredBooks, setFilteredBooks] = useState<BookType[]>([])
   const category = [{ name: "All" }, ...useAppSelector(state => state.Category.category)]
